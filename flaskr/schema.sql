ed9bc3da9179787352989d23c345a33150d62e2f
RyanLeber <ryanleber1@gmail.com>
2023-01-30 18:23:54 -0500

    Added categories table

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS categories;
 
 
 CREATE TABLE user (
@@ -10,11 +11,20 @@ CREATE TABLE user (
 
 CREATE TABLE transactions (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
+  bank TEXT NOT NULL,
   transacted DATE NOT NULL,
   uploaded DATETIME NOT NULL,
   amount DECIMAL NOT NULL,
   [description] TEXT NOT NULL,
-  category TEXT,
+  category_id INTEGER,
+  user_id INTEGER,
+  FOREIGN KEY (category_id) REFERENCES categories (category_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+CREATE TABLE categories (
+  category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  category TEXT
   user_id INTEGER,
   FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
