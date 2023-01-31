e0992654c4985a8a2dfb1698e09b8f3a7702b03e
RyanLeber <ryanleber1@gmail.com>
2023-01-31 16:31:45 -0500

    Added button to edit categories

---
+++ flaskr/static/edit_categories.js
@@ -0,0 +1,14 @@
+document.addEventListener("input", function(){
+    var edit = document.getElementById("can_edit");
+    var x = document.getElementById("summary_table").elements.namedItem("category_input");
+    if (edit.checked == true){
+        for (var i = 0; i < x.length; i++){
+            x[i].readOnly = false;
+        }
+    }
+    else{
+        for (var i = 0; i < x.length; i++){
+            x[i].readOnly = true;
+        }
+    }
+})
