1eaf9f83dcd8df04fc16e05420a254c839e64d6b
RyanLeber <ryanleber1@gmail.com>
2023-01-30 19:23:27 -0500

    Added comments

--- flaskr/categories.py
+++ flaskr/categories.py
@@ -3,13 +3,16 @@ from flask import session
 from flaskr.db import get_db
 from datetime import datetime
 
+# list of fields for the reader to use
 fieldnames = ['date', 'bank', 'amount', 'description', 'type', 'id']
 
 def format_output(filename):
     db = get_db()
 
+    # open parsed csv then read from csv
     with open(filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+        # inputeach row of csv into transactions table
         for row in reader:
             db.execute(
                 'INSERT INTO transactions (transacted, uploaded, bank, amount, description, category, user_id)'
