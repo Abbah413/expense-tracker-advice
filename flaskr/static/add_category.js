6c2d5a7f64c97085671cc9aa5c70a5b24b07e76a
RyanLeber <ryanleber1@gmail.com>
2023-02-02 13:39:01 -0500

    Now correctly adds the proper element

--- flaskr/static/add_category.js
+++ flaskr/static/add_category.js
@@ -2,24 +2,50 @@ function addnew() {
     var table = document.getElementById("categorytable");
     var tbody = table.getElementsByTagName("tbody")[0];
     var row = tbody.insertRow();
-    var cell1 = row.insertCell();
-    var testing = createTest();
-    cell1.appendChild(testing);
+    createCategory(row);
     var cell2 = row.insertCell();
+        cell2.innerHTML = 'budget'
     var cell3 = row.insertCell();
-    var cell4 = row.insertCell();
 }
 
-function createTest(){
+function createCategory(row){
+    var cell1 = row.insertCell();
+    cell1.setAttribute("class", "py-1")
+    var div = document.createElement('DIV');
+    div.setAttribute("class", "input-group py-0");
+    cell1.appendChild(div);
+    var button = buttonAttributes();
+    var input = inputAttributes();
+    div.appendChild(button);
+    div.appendChild(input);
+
+}
+
+function inputAttributes(){
     const attributeDict = {
-        type: "text",
+        type : "text",
         class : "form-control",
         name : "category_input",
         placeholder : "Category",
    };
-   const test = document.createElement("INPUT");
+   const input = document.createElement("INPUT");
     for (const key in attributeDict){
-        test.setAttribute(key, attributeDict[key]);
+        input.setAttribute(key, attributeDict[key]);
    }
-   return test;
+   return input;
+}
+
+function buttonAttributes(){
+    const attributeDict = {
+        type : "button",
+        class : "remove_button"
+    }
+    const button = document.createElement("BUTTON");
+    for (const key in attributeDict){
+        button.setAttribute(key, attributeDict[key]);
+    }
+    var icon = document.createElement('I');
+    icon.setAttribute("class", "bi bi-x-lg");
+    button.appendChild(icon);
+    return button;
 }
