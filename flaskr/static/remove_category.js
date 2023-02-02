b8cbf44a8ff8d5066c30256b8ad7168bd9f2a70a
RyanLeber <ryanleber1@gmail.com>
2023-02-02 14:50:29 -0500

    Edited for better readability

--- flaskr/static/remove_category.js
+++ flaskr/static/remove_category.js
@@ -7,24 +7,32 @@ document.addEventListener('click', e => {
         // stores the category of the clicked row
         let category = parent.nextElementSibling.value
         let data = {action : 'remove', category : category};
-        const index_url = "/"
-        // send the category to the server to be removed from category table
-        fetch(index_url, {
-            headers : {'Content-Type' : 'application/json'},
-            method : 'POST',
-            body : JSON.stringify(data)
-        })
-        .then(response => {
-            // if there is no response back throws  an error
-            if(!response.ok) {
-                throw new Error('Network response not ok')
-            }
-            // finds the parent tr element then remove the row
-            var row = parent.parentNode.parentNode.parentNode;
-            row.parentNode.removeChild(row);
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
+        send_data(data);
+        remove_element(parent);
     }
 });
+
+function send_data(data){
+    const index_url = "/"
+    // send the category to the server to be removed from category table
+    fetch(index_url, {
+        headers : {'Content-Type' : 'application/json'},
+        method : 'POST',
+        body : JSON.stringify(data)
+    })
+    .then(response => {
+        // if there is no response back throws  an error
+        if(!response.ok) {
+            throw new Error('Network response not ok')
+        }
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+// finds the parent tr element then remove the row
+function remove_element(parent){
+    var row = parent.parentNode.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
