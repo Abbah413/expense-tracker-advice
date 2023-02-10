6e0d50c5ab5a7c505f81f124cdf61362c32bf132
RyanLeber <ryanleber1@gmail.com>
2023-02-10 17:39:22 -0500

    added code to initialize tooltips

--- flaskr/static/tooltips.js
+++ flaskr/static/tooltips.js
@@ -1,11 +1,5 @@
-/*
-function incomeTooltip() {
-    let toolTip = document.querySelector("[data-bs-custom-class='custom-tooltip']");
-    toolTip.addEventListener("hover", function(){
-        
-    })
-}
-*/
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+
